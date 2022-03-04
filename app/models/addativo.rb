96de4b65093859439351882dc51cf42bfff71be7
cicerosousacs <93005817+cicerosousacs@users.noreply.github.com>
2022-03-03 22:28:34 -0300

    vinculo_parte2

---
+++ app/models/addativo.rb
@@ -0,0 +1,6 @@
+class Addativo < ApplicationRecord
+  belongs_to :vinculo
+  belongs_to :ativo
+  belongs_to :condicao
+  belongs_to :situacao
+end
---
+++ db/migrate/20220304012755_create_addativos.rb
@@ -0,0 +1,12 @@
+class CreateAddativos < ActiveRecord::Migration[5.2]
+  def change
+    create_table :addativos do |t|
+      t.references :vinculo, foreign_key: true
+      t.references :ativo, foreign_key: true
+      t.references :condicao, foreign_key: true
+      t.references :situacao, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end

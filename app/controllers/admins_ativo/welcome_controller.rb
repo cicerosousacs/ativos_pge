49a5c539a9128901d56de04b75c78baa096e641c
cicerosousacs <93005817+cicerosousacs@users.noreply.github.com>
2022-04-05 23:34:34 -0300

    ok
    ok

--- app/controllers/admins_ativo/welcome_controller.rb
+++ app/controllers/admins_ativo/welcome_controller.rb
@@ -3,8 +3,8 @@ class AdminsAtivo::WelcomeController < AdminsAtivoController
   def index
     @vinculos = Vinculo.all.order("created_at DESC")
     @tipos = Tipo.all
+    @total_ativos = Tipo.sum(:ativos_count)
+    @total_vinculos = Vinculo.count(:id)
   end
-
-
 
 end
--- app/models/tipo.rb
+++ app/models/tipo.rb
@@ -1,4 +1,6 @@
 class Tipo < ApplicationRecord
   has_many :ativos
   paginates_per 10
+
+
 end
